28fbf3ace0361a5cd212a38f1abd1c3e8554e034
without-eyes <dimka180205@gmail.com>
2025-05-01 20:36:21 +0300

    Add FieldOffset enum that stores packet's field offsets

--- include/PcapFileReader.h
+++ include/PcapFileReader.h
@@ -11,6 +11,37 @@
 #include <string>
 #include <pcap/pcap.h>
 
+enum FieldOffset {
+    DESTINATION_MAC_ADDRESS_START = 0,
+    DESTINATION_MAC_ADDRESS_END = 5,
+    SOURCE_MAC_ADDRESS_START = 6,
+    SOURCE_MAC_ADDRESS_END = 11,
+    PROTOCOL_TYPE_START = 12,
+    PROTOCOL_TYPE_END = 13,
+    VERSION_AND_IHL = 14,
+    TYPES_OF_SERVICE = 15,
+    TOTAL_LENGTH_START = 16,
+    TOTAL_LENGTH_END = 17,
+    IDENTIFICATION_NUMBER_START = 18,
+    IDENTIFICATION_NUMBER_END = 19,
+    IP_FLAGS = 20,
+    FRAGMENT_OFFSET_START = 20,
+    FRAGMENT_OFFSET_END = 21,
+    TIME_TO_LIVE = 22,
+    PROTOCOL = 23,
+    HEADER_CHECKSUM_START = 24,
+    HEADER_CHECKSUM_END = 25,
+    SOURCE_IP_ADDRESS_START = 26,
+    SOURCE_IP_ADDRESS_END = 29,
+    DESTINATION_IP_ADDRESS_START = 30,
+    DESTINATION_IP_ADDRESS_END = 33,
+    SOURCE_PORT_START = 34,
+    SOURCE_PORT_END = 35,
+    DESTINATION_PORT_START = 36,
+    DESTINATION_PORT_END = 37,
+    DATA = 38
+};
+
 class PcapFileReader {
 public:
     PcapFileReader() = default;
@@ -21,7 +52,7 @@ public:
 
     void readPacket();
 
-    std::string getMacAddress(int startByte, int endByte) const;
+    std::string getMacAddress(FieldOffset startByte, FieldOffset endByte) const;
 
     std::string getProtocolType() const;
 
--- src/PcapFileReader.cpp
+++ src/PcapFileReader.cpp
@@ -28,7 +28,7 @@ void PcapFileReader::readPacket() {
     }
 }
 
-std::string PcapFileReader::getMacAddress(const int startByte, const int endByte) const {
+std::string PcapFileReader::getMacAddress(const FieldOffset startByte, const FieldOffset endByte) const {
     std::stringstream macAddressStreamString;
     for (int i = startByte; i < endByte; i++) {
         macAddressStreamString << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int>(packet[i]) << ":";
@@ -49,8 +49,8 @@ std::string PcapFileReader::getProtocolType() const {
     }
 
     std::stringstream packetProtocolStringStream;
-    packetProtocolStringStream << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int>(packet[12]);
-    packetProtocolStringStream << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int>(packet[13]);
+    packetProtocolStringStream << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int>(packet[PROTOCOL_TYPE_START]);
+    packetProtocolStringStream << std::setfill('0') << std::setw(2) << std::hex << std::uppercase << static_cast<int>(packet[PROTOCOL_TYPE_END]);
     const std::string packetProtocol = packetProtocolStringStream.str();
 
     std::stringstream resultStringStream;
@@ -61,19 +61,19 @@ std::string PcapFileReader::getProtocolType() const {
 
 int PcapFileReader::getProtocolVersion() const {
     // Get 4 first bites of 15th byte, e.g., 01000101 AND 0b11110000 -> 01000000 >> 4 -> 0100 = 4
-    return (static_cast<int>(packet[14]) & 240) >> 4;
+    return (static_cast<int>(packet[VERSION_AND_IHL]) & 240) >> 4;
 }
 
 int PcapFileReader::getHeaderLength() const {
     // Get 4 last bites of 15th byte, e.g., 01000101 AND 0b00001111 -> 0101 = 5 words = 20 bytes
-    return static_cast<int>(packet[14]) & 15;
+    return static_cast<int>(packet[VERSION_AND_IHL]) & 15;
 }
 
 void PcapFileReader::printPacketInfo() const {
     std::cout << "Size: " << header->len << std::endl;
     std::cout << "Time: " << std::put_time(std::localtime(&header->ts.tv_sec), "%c %Z") << std::endl;
-    std::cout << "Destination MAC: " << getMacAddress(0, 5) << std::endl;
-    std::cout << "Source MAC: " << getMacAddress(6, 11) << std::endl;
+    std::cout << "Destination MAC: " << getMacAddress(DESTINATION_MAC_ADDRESS_START, DESTINATION_MAC_ADDRESS_END) << std::endl;
+    std::cout << "Source MAC: " << getMacAddress(SOURCE_MAC_ADDRESS_START, SOURCE_MAC_ADDRESS_END) << std::endl;
     std::cout << "Type: " << getProtocolType() << std::endl;
     std::cout << "Version: " << std::dec << getProtocolVersion() << std::endl;
     std::cout << "Header Length: " << getHeaderLength() << " (" << getHeaderLength() * 4 << " bytes)" << std::endl;
