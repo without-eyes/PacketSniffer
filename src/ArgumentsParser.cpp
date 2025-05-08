e36aa6e682ebd966ec7a4f29a6a0f3a4d8c802da
without-eyes <dimka180205@gmail.com>
2025-05-08 16:08:56 +0300

    Add '-s' command argument in help output

--- src/ArgumentsParser.cpp
+++ src/ArgumentsParser.cpp
@@ -17,8 +17,8 @@ void ArgumentsParser::parseArguments(const int argc, char *argv[]) {
     auto longOptions = std::make_unique<option[]>(3);
     longOptions[0] = {"help", no_argument, nullptr, 'h'};
     longOptions[1] = {"file", required_argument, nullptr, 'f'};
-    longOptions[1] = {"src", required_argument, nullptr, 's'};
-    longOptions[2] = {nullptr, 0, nullptr, 0};
+    longOptions[2] = {"src", required_argument, nullptr, 's'};
+    longOptions[3] = {nullptr, 0, nullptr, 0};
 
     while ((currentOption = getopt_long(argc, argv, "hf:s:", longOptions.get(), nullptr)) != -1) {
         switch (currentOption) {
@@ -26,6 +26,7 @@ void ArgumentsParser::parseArguments(const int argc, char *argv[]) {
                 std::cout << "Usage: " << argv[0] << " [options] filename" << std::endl;
                 std::cout << "  -h, --help        Show this help message" << std::endl;
                 std::cout << "  -f, --file        Set .pcap file from where packets will be read" << std::endl;
+                std::cout << "  -s, --src         Set filter to print packets with provided source IP address" << std::endl;
                 exit(EXIT_SUCCESS);
 
             case 'f': // read from file
